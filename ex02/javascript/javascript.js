fac06a57b1534d36de9a9a2e9a383d5f57fd4017
Arthur Levy <120432923+Arthur-Llevy@users.noreply.github.com>
2024-08-23 22:43:58 +0000

    adicionado validação para caso o campo seja vazio

--- ex02/javascript/javascript.js
+++ ex02/javascript/javascript.js
@@ -2,8 +2,12 @@ let botaoAdicionar = document.getElementById('botaoAdicionar');
 
 botaoAdicionar.addEventListener('click', () => {
     let numeroDigitado = document.getElementById('numeroDigitado');
-    let somatorio = document.getElementById('somatorio');
-
-    somatorio.value = Number(somatorio.value) + Number(numeroDigitado.value);
+    let somatorio = document.getElementById('somatorio');    
+    
+    if (numeroDigitado.value !== "") {
+        somatorio.value = Number(somatorio.value) + Number(numeroDigitado.value);
+    } else {
+        alert('Digite um número.')        
+    }
 })
 
