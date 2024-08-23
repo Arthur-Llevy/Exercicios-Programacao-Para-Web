fce45337a000fa6e0d8b1b09553c595bc563decd
Arthur Levy <120432923+Arthur-Llevy@users.noreply.github.com>
2024-08-23 23:02:41 +0000

    melhorado a validação do número digitado pelo usuário

--- ex02/javascript/javascript.js
+++ ex02/javascript/javascript.js
@@ -3,11 +3,15 @@ let botaoAdicionar = document.getElementById('botaoAdicionar');
 botaoAdicionar.addEventListener('click', () => {
     let numeroDigitado = document.getElementById('numeroDigitado');
     let somatorio = document.getElementById('somatorio');    
+
+    let validacao = parseInt(numeroDigitado.value)
     
-    if (numeroDigitado.value !== "") {
+    if (!isNaN(validacao)) {
         somatorio.value = Number(somatorio.value) + Number(numeroDigitado.value);
         numeroDigitado.value = "";
         numeroDigitado.focus();
+
+        return;
     } else {
         alert('Digite um número.')        
     }
