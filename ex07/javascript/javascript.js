941cf7846e292d01ef235451c2e1921a348f84d9
Arthur Levy <120432923+Arthur-Llevy@users.noreply.github.com>
2024-09-17 02:29:56 +0000

    lógica do jogo da forca criada

---
+++ ex07/javascript/javascript.js
@@ -0,0 +1,51 @@
+const palavras = [
+    "abacaxi", "anel", "amigo", "ave", "abacate",
+    "bola", "bala", "banho", "bau", "banco",
+    "casa", "cachorro", "carro", "cafe", "cama",
+    "dado", "dedo", "doce", "dia", "dente",
+    "elefante", "estrela", "escola", "elo", "escada",
+    "faca", "festa", "fogo", "foca", "fada",
+    "gato", "galo", "gelo", "goma", "ganso",
+    "helicoptero", "hipopotamo", "hotel", "harpa", "horta",
+    "ilha", "iglu", "iris", "indio", "ima",
+    "janela", "jarra", "jogo", "jumento", "joaninha",
+    "ketchup", "kiwi", "karate", "koala", "kamikaze",
+    "leao", "lago", "lua", "lima", "livro",
+    "maca", "mala", "muro", "mapa", "mesa",
+    "neve", "ninho", "navio", "nuvem", "nota",
+    "olho", "ovo", "onda", "ouro", "orelha",
+    "pato", "peixe", "pipoca", "pato", "perna",
+    "quilo", "quadro", "queijo", "quina", "queda",
+    "raio", "rosa", "rede", "rato", "roupa",
+    "sol", "sapo", "seda", "sabao", "sapato",
+    "tigre", "touro", "teto", "tela", "tesoura",
+    "uva", "urso", "urna", "uniao", "umidade",
+    "vaca", "verao", "vento", "vela", "vidro",
+    "webcam", "whisky", "waffle", "walker", "wifi",
+    "xale", "xadrez", "xerox", "xarope", "xampu",
+    "yoga", "yakisoba", "yogurte", "yeti", "yuppie",
+    "zebra", "zoologico", "zumbi", "zero", "zagueiro"
+];
+
+let palavraAleatoriaEscolhida = palavras[Math.floor(Math.random() * palavras.length)];
+
+const letras = document.getElementById('letras');
+
+console.log(palavraAleatoriaEscolhida)
+
+palavraAleatoriaEscolhida.split('').map(letra => {
+    let letraDaPalavra = document.createElement('p');
+    letraDaPalavra.setAttribute('class', `letra letra-${letra}`)
+    letraDaPalavra.textContent = "";
+    letras.appendChild(letraDaPalavra)
+});
+
+const digitarLetra = (letra) => {
+    if (palavraAleatoriaEscolhida.indexOf(letra.toLowerCase()  ) !== -1) {
+        Array.from(document.getElementsByClassName(`letra-${letra.toLowerCase()}`)).forEach(paragrafo => {
+            paragrafo.textContent = letra
+        });
+    } else {
+        
+    }
+};
